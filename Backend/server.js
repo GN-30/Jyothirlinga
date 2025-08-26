171b18f99e46e082af462bc0896a0c06b22b1e2b
GN-30 <gnsn1200@gmail.com>
2025-08-26 19:51:55 +0530

    doit

--- Backend/server.js
+++ Backend/server.js
@@ -11,8 +11,8 @@ const PORT = process.env.PORT || 3001;
 // This list specifies which frontend URLs are allowed to make requests to this server.
 const allowedOrigins = [
   'http://localhost:5173', // Your local Vite dev environment
-  'http://localhost:3000', // Your local create-react-app dev environment
-  'https://jyothirlinga.vercel.app/' // **IMPORTANT**: Replace this with your actual Vercel URL after deploying the frontend
+   // Your local create-react-app dev environment
+  'https://jyothirlinga.vercel.app' // **IMPORTANT**: Replace this with your actual Vercel URL after deploying the frontend
 ];
 
 const corsOptions = {
